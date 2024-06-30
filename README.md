20aeb20a0f929fde7d89f84a69d1700b17a7b8d4
onasirov <olla_nasirov@intuit.com>
2024-07-01 00:11:52 +0300

    doc

--- README.md
+++ README.md
@@ -4,29 +4,84 @@
 
 ## Solution
 
-
 The service is implemented in [Golang](https://go.dev/), and [Pulumi](https://www.pulumi.com/) is used to deploy the service to AWS EC2.
 I assume the service will not have much idle time as users checking for messages at least once a minute. 
 
-AWS DynamoDB will be used as the database for a messaging system. 
+
+
+### Database
+AWS DynamoDB will be used as the database for the messaging system.
 DynamoDB is a fully managed NoSQL database service that offers high performance, scalability, and low-latency consistency.
 The considerations for cost should be weighed based on the expected workloads, and can be optimized further depending on traffic patterns.
 
-### Additional assumptions:
+##### The database was modeled according to service needs and access patterns.
+- User table: 
+  - userId (string) - HashKey
+  - username (string)
+  - blockedUsers (list of strings)
+- Group table:
+  - groupId (string) - HashKey
+  - groupName (string)
+  - users (list of strings)
+- Message table:
+  - recipientId (string) - HashKey 
+  - timestamp (string) - SortKey
+  - senderId (string)
+  - message (string)
+  
+##### DB access for service calls:
 
-- Any op on non-existing group or user will return error.
-- There is no authentication in the system.
-- Authorization is basic - user can only send message to groups they are part of.
+Most frequent service call:
+- check messages for a user
+  - get user - 1 get call by HashKey
+  - get messages - 1 get call by HashKey+SortKey
 
+Frequent service calls:
+  - send a message to a user
+    - get both users - 2 get calls by HashKey
+    - write message - 1 write call
+  - send a message to a group 
+    - get group and sender user - 2 get calls by HashKey
+    - write message - 1 write call
+
+Rare service calls:
+  - create a new user/group 
+    - write user/group - 1 write call  
+  - add/remove user to group 
+    - get user and group - 2 get calls by HashKey 
+    - update group and user - 2 write call 
+  - block/unblock a user
+    - get both users - 2 get call by HashKey 
+    - update user - 1 write call
+
+##### Caching:
+- Users and group cache - as this data will not change frequently, we can cache the data for a certain period of time, reducing the number of read calls to the database.
+- Messages:
+  - Unlike the users and group tables, messages are updated frequently. To avoid inconsistency, the cache needs to be centralized and should be updated on every write operation. This was not implemented as part of this submission but should be considered if the service is expecting high traffic to improve the performance and reduce cost.
+  - Both direct and group messages are fetched for a user from the DB at once. For caching purposes, as messages from a group are likely to be read by all users of the group, we should cache the group messages to be reused by all users of the group. When fetched by a user, the service will filter and cache the group messages.  This will reduce the number of calls to the database.
+
+With this design, we can optimize the number of calls to the database and reduce the cost of the service.
+- for the most frequent service call, we only need 2 get calls to get the user and messages.
+
+Let's focus on the most frequent service call: 
+As we know every user will check for messages at least once a minute, we can assume that the service will have a high number of read requests which will only depend on the number of users. 
+
+##### Scaling considerations:
+
+
+### Assumptions:
+- As it is not the main focus of the exercise, the service is not following the best security practices:
+  - There is no authentication in the system.
+  - The service is not using HTTPS.
+  - Security groups settings are not optimized.
+  - Authorization is basic and limited - for example user can only send message to groups they are part of.
+  - There is no rate limiting.
+  - And more...
+
+#### Functionality assumptions:
 *User*
 - No authorization needed to create a user, nor blocking or unblocking users.
 - User-name is not unique.
-
-*Block*
-- Blocking already blocked user will return error.
-- Block user will block from sending direct messages only. Blocked user can still send messages to groups they are part of.
-- Blocked user will get forbidden error when trying to send a private message.
-- User can block itself.
 
 *Group*
 - No authorization needed to create a group, nor adding or removing users to group.
@@ -34,6 +89,12 @@ The considerations for cost should be weighed based on the expected workloads, a
 - If user is already in the group, adding again will return error.
 - If user is not in the group, removing will return error.
 
+*Block*
+- Blocking already blocked user will return error.
+- Block user will block from sending direct messages only. Blocked user can still send messages to groups they are part of.
+- Blocked user will get forbidden error when trying to send a private message.
+- User can block itself.
+- 
 *Message*
 - User can send message to self, other users and groups it is part of.
 - If user have no messages, the response will contain null message array.
--- main.go
+++ main.go
@@ -1,68 +1,78 @@
 package main
 
 import (
+	"fmt"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/dynamodb"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ecs"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/iam"
+	"github.com/pulumi/pulumi-awsx/sdk/v2/go/awsx/awsx"
 	ecsx "github.com/pulumi/pulumi-awsx/sdk/v2/go/awsx/ecs"
 	"github.com/pulumi/pulumi-awsx/sdk/v2/go/awsx/lb"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"os"
 )
 
 func main() {
+
+	// read aws account from env var AWS_ACCOUNT_ID
+	awsAccount := os.Getenv("AWS_ACCOUNT_ID")
+	imageName := fmt.Sprintf("%s.dkr.ecr.us-west-2.amazonaws.com/messaging-system-app:latest", awsAccount)
+
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create a new DynamoDB table
-		//_, err := dynamodb.NewTable(ctx, "messagesTable", &dynamodb.TableArgs{
-		//	Attributes: dynamodb.TableAttributeArray{
-		//		&dynamodb.TableAttributeArgs{
-		//			Name: pulumi.String("RecipientId"),
-		//			Type: pulumi.String("S"),
-		//		},
-		//		&dynamodb.TableAttributeArgs{
-		//			Name: pulumi.String("Timestamp"),
-		//			Type: pulumi.String("S"),
-		//		},
-		//	},
-		//	HashKey:        pulumi.String("RecipientId"),
-		//	RangeKey:       pulumi.String("Timestamp"),
-		//	BillingMode:    pulumi.String("PAY_PER_REQUEST"),
-		//	StreamEnabled:  pulumi.Bool(true),
-		//	StreamViewType: pulumi.String("NEW_AND_OLD_IMAGES"),
-		//})
-		//
-		//if err != nil {
-		//	return err
-		//}
-		//
-		//_, err = dynamodb.NewTable(ctx, "usersTable", &dynamodb.TableArgs{
-		//	Attributes: dynamodb.TableAttributeArray{
-		//		&dynamodb.TableAttributeArgs{
-		//			Name: pulumi.String("UserId"),
-		//			Type: pulumi.String("S"),
-		//		},
-		//	},
-		//	HashKey:        pulumi.String("UserId"),
-		//	BillingMode:    pulumi.String("PAY_PER_REQUEST"),
-		//	StreamEnabled:  pulumi.Bool(true),
-		//	StreamViewType: pulumi.String("NEW_AND_OLD_IMAGES"),
-		//})
-		//if err != nil {
-		//	return err
-		//}
-		//
-		//_, err = dynamodb.NewTable(ctx, "groupsTable", &dynamodb.TableArgs{
-		//	Attributes: dynamodb.TableAttributeArray{
-		//		&dynamodb.TableAttributeArgs{
-		//			Name: pulumi.String("GroupId"),
-		//			Type: pulumi.String("S"),
-		//		},
-		//	},
-		//	HashKey:        pulumi.String("GroupId"),
-		//	BillingMode:    pulumi.String("PAY_PER_REQUEST"),
-		//	StreamEnabled:  pulumi.Bool(true),
-		//	StreamViewType: pulumi.String("NEW_AND_OLD_IMAGES"),
-		//})
-		//if err != nil {
-		//	return err
-		//}
+		// Create required DynamoDB tables
+		_, err := dynamodb.NewTable(ctx, "messagesTable", &dynamodb.TableArgs{
+			Attributes: dynamodb.TableAttributeArray{
+				&dynamodb.TableAttributeArgs{
+					Name: pulumi.String("RecipientId"),
+					Type: pulumi.String("S"),
+				},
+				&dynamodb.TableAttributeArgs{
+					Name: pulumi.String("Timestamp"),
+					Type: pulumi.String("S"),
+				},
+			},
+			HashKey:        pulumi.String("RecipientId"),
+			RangeKey:       pulumi.String("Timestamp"),
+			BillingMode:    pulumi.String("PAY_PER_REQUEST"),
+			StreamEnabled:  pulumi.Bool(true),
+			StreamViewType: pulumi.String("NEW_AND_OLD_IMAGES"),
+		})
+
+		if err != nil {
+			return err
+		}
+
+		_, err = dynamodb.NewTable(ctx, "usersTable", &dynamodb.TableArgs{
+			Attributes: dynamodb.TableAttributeArray{
+				&dynamodb.TableAttributeArgs{
+					Name: pulumi.String("UserId"),
+					Type: pulumi.String("S"),
+				},
+			},
+			HashKey:        pulumi.String("UserId"),
+			BillingMode:    pulumi.String("PAY_PER_REQUEST"),
+			StreamEnabled:  pulumi.Bool(true),
+			StreamViewType: pulumi.String("NEW_AND_OLD_IMAGES"),
+		})
+		if err != nil {
+			return err
+		}
+
+		_, err = dynamodb.NewTable(ctx, "groupsTable", &dynamodb.TableArgs{
+			Attributes: dynamodb.TableAttributeArray{
+				&dynamodb.TableAttributeArgs{
+					Name: pulumi.String("GroupId"),
+					Type: pulumi.String("S"),
+				},
+			},
+			HashKey:        pulumi.String("GroupId"),
+			BillingMode:    pulumi.String("PAY_PER_REQUEST"),
+			StreamEnabled:  pulumi.Bool(true),
+			StreamViewType: pulumi.String("NEW_AND_OLD_IMAGES"),
+		})
+		if err != nil {
+			return err
+		}
 
 		lb, err := lb.NewApplicationLoadBalancer(ctx, "lb", nil)
 		if err != nil {
@@ -74,32 +84,72 @@ func main() {
 			return err
 		}
 
-		//repository, err := ecr.NewRepository(ctx, "repository", &ecr.RepositoryArgs{
-		//	ForceDelete: pulumi.Bool(true),
-		//})
-		//if err != nil {
-		//	return err
-		//}
-		//
-		//image, err := ecr.NewImage(ctx, "image", &ecr.ImageArgs{
-		//
-		//	RepositoryUrl: repository.Url,
-		//	Context:       pulumi.String("./server"),
-		//	//Platform:      pulumi.String("linux/amd64"),
-		//})
-		//if err != nil {
-		//	return err
-		//}
+		// Create IAM Role and Policy for Fargate Task
+		role, err := iam.NewRole(ctx, "taskRole", &iam.RoleArgs{
+			AssumeRolePolicy: pulumi.String(`{
+				"Version": "2012-10-17",
+				"Statement": [
+					{
+						"Effect": "Allow",
+						"Principal": {
+							"Service": "ecs-tasks.amazonaws.com"
+						},
+						"Action": "sts:AssumeRole"
+					}
+				]
+			}`),
+		})
+		if err != nil {
+			return err
+		}
+
+		policy, err := iam.NewPolicy(ctx, "taskPolicy", &iam.PolicyArgs{
+			Description: pulumi.String("Policy for Fargate task to access DynamoDB"),
+			Policy: pulumi.String(
+				fmt.Sprintf(`{
+					"Version": "2012-10-17",
+					"Statement": [
+						{
+							"Effect": "Allow",
+							"Action": [
+								"dynamodb:DescribeTable",
+								"dynamodb:Query",
+								"dynamodb:Scan",
+								"dynamodb:GetItem",
+								"dynamodb:PutItem",
+								"dynamodb:UpdateItem",
+								"dynamodb:DeleteItem"
+							],
+							
+							"Resource": "*"
+						}
+					]
+				}`),
+			),
+		})
+		if err != nil {
+			return err
+		}
+
+		_, err = iam.NewRolePolicyAttachment(ctx, "taskPolicyAttachment", &iam.RolePolicyAttachmentArgs{
+			PolicyArn: policy.Arn,
+			Role:      role.Name,
+		})
+		if err != nil {
+			return err
+		}
 
 		_, err = ecsx.NewFargateService(ctx, "messaging-system-service", &ecsx.FargateServiceArgs{
 			Cluster:        cluster.Arn,
 			AssignPublicIp: pulumi.Bool(true),
-			DesiredCount:   pulumi.Int(2),
+			DesiredCount:   pulumi.Int(1),
 			TaskDefinitionArgs: &ecsx.FargateServiceTaskDefinitionArgs{
+				TaskRole: &awsx.DefaultRoleWithPolicyArgs{
+					RoleArn: role.Arn,
+				},
 				Container: &ecsx.TaskDefinitionContainerDefinitionArgs{
-
 					Name:      pulumi.String("messaging-system"),
-					Image:     pulumi.String("339713044858.dkr.ecr.us-west-2.amazonaws.com/messaging-system-app:0.0.13"),
+					Image:     pulumi.String(imageName),
 					Cpu:       pulumi.Int(512),
 					Memory:    pulumi.Int(1042),
 					Essential: pulumi.Bool(true),
@@ -263,6 +313,23 @@ func main() {
 		//		// Export the public IP of the instance
 		//		ctx.Export("publicIp", server.PublicIp)
 		//		ctx.Export("publicDns", server.PublicDns)
+
+		//repository, err := ecr.NewRepository(ctx, "repository", &ecr.RepositoryArgs{
+		//	ForceDelete: pulumi.Bool(true),
+		//})
+		//if err != nil {
+		//	return err
+		//}
+		//
+		//image, err := ecr.NewImage(ctx, "image", &ecr.ImageArgs{
+		//
+		//	RepositoryUrl: repository.Url,
+		//	Context:       pulumi.String("./server"),
+		//	//Platform:      pulumi.String("linux/amd64"),
+		//})
+		//if err != nil {
+		//	return err
+		//}
 
 		return nil
 
